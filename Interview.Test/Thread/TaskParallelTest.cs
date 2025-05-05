e4d92e6562042f455d5ee87ade97775d1eccf8e0
yosheng <sad445566@gmail.com>
2025-05-05 22:04:50 +0800

    feat: 添加多线程和锁使用

---
+++ Interview.Test/Thread/TaskParallelTest.cs
@@ -0,0 +1,52 @@
+﻿namespace Interview.Test.Thread;
+
+using System.Threading;
+
+public class TaskParallelTest : XunitContextBase
+{
+    [Fact]
+    public void Test()
+    {
+        Action();
+        ActionLock();
+        ActioniInterLock();
+    }
+    
+    public void Action()
+    {
+        int a = 0;
+        Parallel.For(0, 100000, (i) =>
+        {
+            a++;
+        });
+        Console.WriteLine($"总数{a}.....{Thread.CurrentThread.ManagedThreadId}");
+    }
+    
+    public void ActionLock()
+    {
+        var obj = new Object();
+        int a = 0;
+        Parallel.For(0, 100000, (i) =>
+        {
+            lock (obj)
+            {
+                a++;
+            }
+        });
+        Console.WriteLine($"加锁總數{a}.....{Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    public void ActioniInterLock()
+    {
+        int a = 0;
+        Parallel.For(0, 100000, (i) =>
+        {
+            Interlocked.Increment(ref a);
+        });
+        Console.WriteLine($"InterLocked總數{a}.....{Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    public TaskParallelTest(ITestOutputHelper output) : base(output)
+    {
+    }
+}
---
+++ Interview.Test/Thread/TaskSemaphoreSlimTest.cs
@@ -0,0 +1,45 @@
+﻿namespace Interview.Test.Thread;
+
+using System.Threading;
+
+public class TaskSemaphoreSlimTest : XunitContextBase
+{
+    static SemaphoreSlim _semaphore = new SemaphoreSlim(2);
+    
+    [Fact]
+    public void Test()
+    {
+        Action();
+    }
+    
+    public static void Action()
+    {
+        Console.WriteLine($"{DateTime.Now}开始.....{Thread.CurrentThread.ManagedThreadId}");
+        Task t1 = Task.Run(() => DoWork(1));
+        Task t2 = Task.Run(() => DoWork(2));
+        Task t3 = Task.Run(() => DoWork(3));
+        Task t4 = Task.Run(() => DoWork(4));
+
+        Task.WaitAll(t1, t2, t3, t4);
+        Console.WriteLine($"{DateTime.Now}结束.....{Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    static void DoWork(int id)
+    {
+        _semaphore.Wait();
+
+        try
+        {
+            Console.WriteLine($"{DateTime.Now}启动任务{id}.....{Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(2000);
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+    
+    public TaskSemaphoreSlimTest(ITestOutputHelper output) : base(output)
+    {
+    }
+}
