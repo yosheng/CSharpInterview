7d8c494d5affe24c807e2b9e86b794c7bccecef8
yosheng <sad445566@gmail.com>
2025-05-10 17:49:53 +0800

    feat: 添加委托闭包题

---
+++ Interview.Test/Advance/ClosureTest.cs
@@ -0,0 +1,39 @@
+﻿namespace Interview.Test.Advance;
+
+public class ClosureTest : XunitContextBase
+{
+    public static void Action()
+    {
+        var person = new Person()
+        {
+            Name = "John",
+            Age = 1
+        };
+
+        var actions = new List<Action>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            actions.Add(() =>
+            {
+                person.Age = person.Age * i;
+                Console.WriteLine($"age: {person.Age}");
+            });
+        }
+        
+        foreach (var action in actions)
+        {
+            action.Invoke();
+        }
+    }
+    
+    [Fact]
+    public void Test()
+    {
+        Action();
+    }
+    
+    public ClosureTest(ITestOutputHelper output) : base(output)
+    {
+    }
+}
---
+++ Interview.Test/Advance/DelegateTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+
+namespace Interview.Test.Advance;
+
+public class DelegateTest : XunitContextBase
+{
+    [Fact]
+    public void Test1()
+    {
+        Exec();
+    }
+
+
+    delegate double MyDelegate(int num);
+
+    public static void Exec()
+    {
+        var myDelegate = new MyDelegate(Run1);
+        myDelegate += Run2;
+        Console.WriteLine($"结果: {myDelegate(100)}");
+    }
+
+    static double Run1(int num)
+    {
+        var result = num * 0.1; 
+        Console.WriteLine($"Run1-{result}");
+        return result;
+    }
+    
+    static double Run2(int num)
+    {
+        var result = num * 0.2;
+        Console.WriteLine($"Run2-{result}");
+        return result;
+    }
+
+    public DelegateTest(ITestOutputHelper output) : base(output)
+    {
+    }
+}
